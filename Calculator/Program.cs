b810d00dbd5ada824e3ebeb4b1a5c35a17f97b23
ypron <ypron@LAPTOP-5H1KDVCM>
2025-08-24 23:49:44 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class ConsolCalculator
+{
+    public double number1;
+    public double number2;
+    public char sign;
+
+    public ConsolCalculator() { }
+    public void Сalculation()
+    {
+        Console.WriteLine("ВВЕДИТЕ ВЫРАЖЕНИЕ ДЛЯ ВЫЧИСЛЕНИЯ:");
+        string str = Console.ReadLine();
+        char[] separators = { '+', '-', '*', '/', '%', '=' };
+        for (int i = 0; i < str.Length; i++)
+        {
+            for (int j = 0; j < separators.Length; j++)
+            {
+                char c = str[i];
+
+                if (c == separators[j]) { sign = separators[j]; }
+            }
+
+        }
+        Console.WriteLine($"{sign}");
+        string[] values = str.Split(separators);
+        double number1 = Convert.ToDouble(values[0]);
+        double number2 = Convert.ToDouble(values[1]);
+
+        switch (sign)
+        {
+            case '+': Console.WriteLine($"{number1} {sign} {number2} = {number1 + number2}"); break;
+            case '-': Console.WriteLine($"{number1} {sign} {number2} = {number1 - number2}"); break;
+            case '*': Console.WriteLine($"{number1} {sign} {number2} = {number1 * number2}"); break;
+            case '/': Console.WriteLine($"{number1} {sign} {number2} = {number1 / number2}"); break;
+
+        }
+
+    }
+}
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsolCalculator cina = new ConsolCalculator();
+            cina.Сalculation();
+        
+        }
+    }
+}
---
+++ IntroductionToDOTNET/Program.cs
@@ -0,0 +1,58 @@
+﻿//#define STRING_OPERATIONS
+//#define DATA_TAYPS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace IntroductionToDOTNET
+{
+    internal class Program
+    {
+        class Factorial
+        {
+  
+            public int num {  get; set; }
+           
+
+        }
+        static void Main(string[] args)
+        {
+#if STRING_OPERATIONS
+            //Console.Write("Hello .NET\n");
+            //Console.Write("Привет");
+            //Console.WriteLine("Hello .NET");
+
+            Console.Write("Введите ваше имя: ");
+            string first_name = Console.ReadLine();
+
+            Console.Write("Введите вашу фамилию: ");
+            string last_name = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(first_name + " " + last_name + " " + age); //конкатенация строк
+
+            Console.WriteLine(String.Format("{0} {1} {2}", first_name, last_name, age));//форматирование строк
+
+            Console.WriteLine($"{first_name} {last_name} {age}");//интерполяция строк  
+#endif
+
+#if DATA_TAYPS
+            Console.WriteLine(sizeof(bool));
+            //Console.WriteLine(Boolean.);
+            bool var1;
+            Boolean var2;
+            //using DataType= short;
+            short var3 = 0;
+            Console.WriteLine($"{var3.GetType()} занимает {sizeof(short)} байт памяти и принимает значение от  {short.MinValue} ...{short.MaxValue}");
+#endif
+
+          
+
+        }
+    }
+}
